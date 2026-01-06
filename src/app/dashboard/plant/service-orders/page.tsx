f4a9cd7724e6daac985b971bad870a82185e7b71
margh00b <marghoobahmd25@gmail.com>
2026-01-06 16:46:47 -0700

    fix plant so permissisons

--- src/app/dashboard/plant/service-orders/page.tsx
+++ src/app/dashboard/plant/service-orders/page.tsx
@@ -5,25 +5,5 @@ import { usePermissions } from "@/hooks/usePermissions";
 import { Center, Loader, Text } from "@mantine/core";
 
 export default function Page() {
-  const permissions = usePermissions();
-
-  if (!permissions.isLoaded) {
-    return (
-      <Center h="100vh">
-        <Loader />
-      </Center>
-    );
-  }
-
-  if (!permissions.isPlant && !permissions.isAdmin) {
-    return (
-      <Center h="100vh">
-        <Text c="red" fw={700}>
-          Unauthorized Access
-        </Text>
-      </Center>
-    );
-  }
-
   return <PlantServiceOrdersTable />;
 }
