f730bbeac217923178da7550c04e134a28634405
margh00b <marghoobahmd25@gmail.com>
2026-01-09 12:50:28 -0700

    Fix Outlook Auth

--- src/components/Outlook/Inbox/OutlookAuth.tsx
+++ src/components/Outlook/Inbox/OutlookAuth.tsx
@@ -8,43 +8,62 @@ import {
 } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 
-export const msalConfig = {
+const msalConfig = {
   auth: {
     clientId: "3ea61d91-6068-4812-aadf-d4735e9992e0",
     authority: "https://login.microsoftonline.com/common",
     redirectUri: process.env.NEXT_PUBLIC_MSAL_REDIRECT_URI,
+    navigateToLoginRequestUrl: true,
   },
   cache: {
     cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false,
   },
 };
 
 const msalInstance = new PublicClientApplication(msalConfig);
-
-const accounts = msalInstance.getAllAccounts();
-if (accounts.length > 0) {
-  msalInstance.setActiveAccount(accounts[0]);
-}
-
-msalInstance.addEventCallback((event) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-    const payload = event.payload as AuthenticationResult;
-    msalInstance.setActiveAccount(payload.account);
-  }
-});
 
 export default function OutlookAuthProvider({
   children,
 }: {
   children: ReactNode;
 }) {
-  const [isInitialized, setIsInitialized] = useState(false);
+  const [isMsalInitialized, setIsMsalInitialized] = useState(false);
 
   useEffect(() => {
-    setIsInitialized(true);
+    const initializeMsal = async () => {
+      try {
+        await msalInstance.initialize();
+
+        await msalInstance.handleRedirectPromise().then((response) => {
+          if (response) {
+            console.log("Login Success via Redirect:", response);
+            msalInstance.setActiveAccount(response.account);
+          }
+        });
+
+        const accounts = msalInstance.getAllAccounts();
+        if (accounts.length > 0 && !msalInstance.getActiveAccount()) {
+          msalInstance.setActiveAccount(accounts[0]);
+        }
+
+        msalInstance.addEventCallback((event) => {
+          if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+            const payload = event.payload as AuthenticationResult;
+            msalInstance.setActiveAccount(payload.account);
+          }
+        });
+
+        setIsMsalInitialized(true);
+      } catch (error) {
+        console.error("MSAL Initialization Failed:", error);
+      }
+    };
+
+    initializeMsal();
   }, []);
 
-  if (!isInitialized) return null;
+  if (!isMsalInitialized) return null;
 
   return <MsalProvider instance={msalInstance}>{children}</MsalProvider>;
 }
